e80c5b1a5307329fa5868e871adeff740714ddb3
Oliver Cappis <Oliver_Cappis1@baylor.edu>
2024-01-28 15:24:09 -0600

    solved problems

--- script1.py
+++ script1.py
@@ -3,14 +3,14 @@
 # Johnny, the total of your purchase is $1,688.70
 
 def calcTotal(amt,tax,name):
-    mytotal = amt * (1+tax)
-    print(f"{myname}, the total of your purchase is ${total:,.2f}")
+     mytotal = amt * (1+tax)
+     print(f"{myname}, the total of your purchase is ${mytotal:,.2f}")
 
 myname = 'Johnny'
-price = 1,560
-thetax = 8.25%
+price = 1560
+thetax = .0825
 
-calcTotal(thetax,price,name)
+calcTotal(price,thetax,myname)
 
 
 
@@ -21,7 +21,8 @@ calcTotal(thetax,price,name)
 list1 = [1,'two',3.2,'four',5]
 
 for i in list1:
-    print(i)
+    if isinstance(i, int):
+        print(i)
 
 
 
@@ -32,6 +33,7 @@ for i in list1:
 # the filename is Baylor.html and it should
 # open up in your browser. The library is Plotly.
 
+import plotly
 from plotly.graph_objs import Scattergeo, Layout
 from plotly import offline
 
